aaa4c6bbe68b01c03e75b57233d6906cdd50e1d0
League Student <league@iMac-12.attlocal.net>
2019-12-28 11:27:09 -0800

    robot race (ip)

--- src/_01_Olympic_Rings/OlympicRings_Threaded.java
+++ src/_01_Olympic_Rings/OlympicRings_Threaded.java
@@ -1,8 +1,74 @@
 package _01_Olympic_Rings;
 
+import java.awt.Graphics2D;
+
+import org.jointheleague.graphical.robot.Robot;
 
 public class OlympicRings_Threaded {
 	// Make A Program that uses Threads and robots to draw the Olympic rings. One robot should draw one ring simultaneously with the other 4 robots.
-
+	public static void main(String[] args) {
+		Robot r1 = new Robot(150, 300);
+		Robot r2 = new Robot(400, 600);
+		Robot r3 = new Robot(600, 300);
+		Robot r4 = new Robot(850, 600);
+		Robot r5 = new Robot(1000, 300);
+		
+		r1.setSpeed(10);
+		r1.penDown();
+		r2.setSpeed(10);
+		r2.penDown();
+		r3.setSpeed(10);
+		r3.penDown();
+		r4.setSpeed(10);
+		r4.penDown();
+		r5.setSpeed(10);
+		r5.penDown();
+		
+		r1.penDown();
+		
+		
+		Thread m1 = new Thread(()->{
+			for(int i = 0; i < 20; i++) {
+				r1.move(75);
+				r1.turn(20);
+			}
+		});
+		
+		Thread m2 = new Thread(()->{
+			for(int i = 0; i < 20; i++) {
+				r2.move(75);
+				r2.turn(20);
+			}
+		});
+		
+		
+		Thread m3 = new Thread(()->{
+			for(int i = 0; i < 20; i++) {
+				r3.move(75);
+				r3.turn(20);
+			}
+		});
+		
+		Thread m4 = new Thread(()->{
+			for(int i = 0; i < 20; i++) {
+				r4.move(75);
+				r4.turn(20);
+			}
+		});
+		
+		Thread m5 = new Thread(()->{
+			for(int i = 0; i < 20; i++) {
+				r5.move(75);
+				r5.turn(20);
+			}
+		});
+		
+		m1.start();
+		m2.start();
+		m3.start();
+		m4.start();
+		m5.start();
+		
+	}
 }
 
--- src/_02_Advanced_Robot_Race/AdvancedRobotRace.java
+++ src/_02_Advanced_Robot_Race/AdvancedRobotRace.java
@@ -1,6 +1,49 @@
 package _02_Advanced_Robot_Race;
 
+import java.util.Random;
+
+import org.jointheleague.graphical.robot.Robot;
+
 public class AdvancedRobotRace {
 	// Re-do the robot race recipe from level 3 module 0. 
 	// This time, use threads to make all of the robots go at the same time.
+	
+	
+	public static void main(String[] args) {
+		
+		Robot[] r = new Robot[5];
+		int add = 75;
+		for(int i = 0; i < r.length; i++) {
+			r[i] = new Robot();
+			r[i].setSpeed(100);
+			r[i].setX(add);
+			r[i].setY(550);
+			add+=800/r.length;
+		}
+		
+		Thread m1 = new Thread(()->{
+			for(int i = 0; i < 20; i++) {
+				r1.move(75);
+				r1.turn(20);
+			}
+		});
+
+		Random rand = new Random();
+		boolean finished = false;
+		int winner = 0;
+		while(!finished) {
+			for(int i =0; i < r.length; i++) {
+				if(r[i].getY() > 0) {
+					r[i].move(rand.nextInt(50));
+				}else {
+					winner = i+1;
+					finished = true;
+					break;
+				}
+			}
+		}
+		
+		System.out.println("Robot " + winner + " won the race");
+		
+	}
 }
